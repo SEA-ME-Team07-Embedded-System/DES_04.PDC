658928ad02102fd632089e97aecaf29809561916
Junho Kim <73748884+junho2000@users.noreply.github.com>
2023-11-21 15:18:17 +0100

    initial commit and setting for ivi compositor

---
+++ ivi-compositor/build/CMakeFiles/wayland_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wayland_autogen"
+  "wayland_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/wayland_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
