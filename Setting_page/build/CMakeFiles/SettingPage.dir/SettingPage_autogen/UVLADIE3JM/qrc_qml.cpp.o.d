5513f15905bab4d9faa1f05be2ecc4304c808752
junho2000 <jhk00@kookmin.ac.kr>
2024-01-11 11:35:25 -0500

    Update Setting page to change gear on page

---
+++ Setting_page/build/CMakeFiles/SettingPage.dir/SettingPage_autogen/UVLADIE3JM/qrc_qml.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/SettingPage.dir/SettingPage_autogen/UVLADIE3JM/qrc_qml.cpp.o: \
+ /home/junho/Desktop/DES_04.PDC/Setting_page/build/SettingPage_autogen/UVLADIE3JM/qrc_qml.cpp \
+ /usr/include/stdc-predef.h
---
+++ Setting_page/build/CMakeFiles/SettingPage_autogen.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for SettingPage_autogen.
+# This may be replaced when dependencies are built.
