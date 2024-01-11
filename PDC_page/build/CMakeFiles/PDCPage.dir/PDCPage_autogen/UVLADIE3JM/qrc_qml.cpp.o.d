e45ff04ab5eb0ea96a621954c03991cc905c1178
junho2000 <jhk00@kookmin.ac.kr>
2024-01-11 11:41:59 -0500

    Update PDC page

---
+++ PDC_page/build/CMakeFiles/PDCPage.dir/PDCPage_autogen/UVLADIE3JM/qrc_qml.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/PDCPage.dir/PDCPage_autogen/UVLADIE3JM/qrc_qml.cpp.o: \
+ /home/junho/Desktop/DES_04.PDC/PDC_page/build/PDCPage_autogen/UVLADIE3JM/qrc_qml.cpp \
+ /usr/include/stdc-predef.h
---
+++ PDC_page/build/CMakeFiles/PDCPage_autogen.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for PDCPage_autogen.
+# This may be replaced when dependencies are built.
